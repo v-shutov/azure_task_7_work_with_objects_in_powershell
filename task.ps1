5a14c6479ce78870aacfae6b8d8df958d4fa96f6
Volodymyr Shutov <forshutov@gmail.com>
2024-08-21 16:47:02 +0400

    Solution

--- task.ps1
+++ task.ps1
@@ -1,1 +1,23 @@
-# Write your code here
+$inputPath = "$PWD/data/"
+$outputPath = "$PWD/result.json"
+$searchString = "Standard_B2pts_v2"
+
+if (-Not (Test-Path -Path $inputPath)) {
+    Write-Error "$inputPath does not exist."
+    exit
+}
+
+$jsonFiles = Get-ChildItem -Path $inputPath -Filter *.json
+
+$regionList = @()
+
+foreach ($file in $jsonFiles) {
+    $jsonContent = Get-Content -Path $file.FullName -Raw
+    $jsonObject = $jsonContent | ConvertFrom-Json
+
+    if ($jsonObject.Name -eq $searchString) {
+        $regionList += $file.BaseName
+    }
+}
+
+$regionList | ConvertTo-Json | Out-File -Path $outputPath
